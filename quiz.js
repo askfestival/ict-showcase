c5661b561918dd01b771e4e404f3202abe7b9b80
ict-room5 <zidane_rupani@outlook.com>
2026-01-27 16:52:57 +0530

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,45 +1,43 @@
-function loadQuiz(questions){
+function loadQuiz(questions) {
     const quiz = document.getElementById("quiz");
     quiz.innerHTML = "";
 
-    questions.forEach((q, i)=>{
+    questions.forEach((q, i) => {
         const div = document.createElement("div");
         div.className = "question";
 
         const p = document.createElement("p");
-        p.innerText = (i+1) + ". " + q.q;
+        p.innerText = (i + 1) + ". " + q.q;
         div.appendChild(p);
 
-        q.options.forEach((opt, idx)=>{
+        q.options.forEach((opt, idx) => {
             const btn = document.createElement("button");
             btn.className = "option";
             btn.innerText = opt;
 
-            if(idx === q.answer){
+            if (idx === q.answer) {
                 btn.dataset.correct = "true";
             }
 
             btn.onclick = () => {
-                if(div.classList.contains("answered")) return;
+                if (div.classList.contains("answered")) return;
                 div.classList.add("answered");
 
                 const buttons = div.querySelectorAll(".option");
 
-                buttons.forEach(b=>{
-                    if(b.dataset.correct){
+                buttons.forEach(b => {
+                    if (b.dataset.correct) {
                         b.classList.add("correct");
-                    }else{
+                    } else {
                         b.classList.add("wrong");
                     }
                 });
 
-                if(!btn.dataset.correct){
+                if (!btn.dataset.correct) {
                     btn.classList.add("selected");
-                }else{
-                    buttons.forEach(b=>{
-                        if(b !== btn){
-                            b.classList.add("grey");
-                        }
+                } else {
+                    buttons.forEach(b => {
+                        if (b !== btn) b.classList.add("grey");
                     });
                 }
             };
